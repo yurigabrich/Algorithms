fc345bab32e602579f99e67e437fc8baebeb768f
Yuri <yuribgabrich@gmail.com>
2017-02-08 15:50:08 -0200

    Update pos_ordem.c

--- pos_ordem.c
+++ pos_ordem.c
@@ -5,3 +5,12 @@ void posordem (No∗ no) {
     printf("%d\n", no−>valor);
 	}
 };
+
+procedimento PercursoPosOrdem(T: ^No)
+	se T ≠ NULO então
+	PercursoPosOrdem(T^.Esq)
+	PercursoPosOrdem(T^.Dir)
+	escrever (T^.Chave)
+
+procedimento PercursoPosOrdem(ref T: Arvore)
+	PercursoPosOrdem(T.Raiz)
