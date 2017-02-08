d06adfa499d0f74e9e2125f5247481303fe76339
Yuri <yuribgabrich@gmail.com>
2017-02-08 15:40:15 -0200

    Create binary_tree.c

---
+++ binary_tree.c
@@ -0,0 +1,18 @@
+class No {
+	public:
+	int valor; // conteúdo do nó
+	No∗ esq; // filho esquerdo
+	No∗ dir; // filho direito
+	No∗ pai; // opcional
+};
+
+class ArvoreBinaria {
+	public:
+	No∗ raiz;
+	int N; // número de elementos atualmente na pilha?!
+	void constroi(){...}
+	
+	void destroi(){...}
+	
+	void imprime(){...}
+}
