aba7cb6f5067ae6bbc3ea3a2b44710c0dd907a01
Yuri <yuribgabrich@gmail.com>
2017-02-08 15:50:47 -0200

    Update em_ordem.c

--- em_ordem.c
+++ em_ordem.c
@@ -5,3 +5,12 @@ void emordem (No∗ no) {
 		preordem(no−>dir);
 	}
 };
+
+procedimento PercursoInOrdem(T: ^No)
+	se T ≠ NULO então
+	PercursoInOrdem(T^.Esq)
+	escrever (T^.Chave)
+	PercursoInOrdem(T^.Dir)
+
+procedimento PercursoInOrdem(ref T: Arvore)
+	PercursoInOrdem(T.Raiz)
