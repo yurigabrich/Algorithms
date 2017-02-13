6175439ba3e235e4535adfacbb585a3f70591af9
Yuri <yuribgabrich@gmail.com>
2017-02-13 17:41:45 -0200

    Update ListaSeq.c

--- C_files/ListaSeq.c
+++ C_files/ListaSeq.c
@@ -2,6 +2,7 @@
 #define MAXN = 100000
 
 typedef struct {
+	/* Nó Sequencial */
 	char chave;
 	char valor;
 } NoSeq;
@@ -11,24 +12,59 @@ typedef struct {
 	int N;
 
 	void constroi() {
-		/* inicializa a lista para uso */
+		/* Inicializa a lista para uso. */
 		N = 0;
 	}
 
 	void destroi() {
-		/* libera os recursos requisitados */
+		/* Libera os recursos requisitados. */
 		// vazio
 	}
-
+	
+	// Busca NÃO-ORDENADA
 	int busca(char chave) {
-		/*  */
+		/* Retorna a colocação do NoSeq contendo a chave,
+		ou -1 caso NoSeq não esteja na lista. */
+		for (int i = 0; i < N; ++i) {
+			if (elementos[i].chave == chave) {
+				return i;
+		return -1;
+	}
+	// Busca ORDENADA
+	int buscabinaria(char chave) {
+		return buscarec(chave, 0, N-1);
+	}
+	int buscarec(char chave, int esquerda, int direita) {
+		if (esqueda > direita) {
+			return -1;
+		}
+		int pivo = (esquerda + direita) / 2;
+		
+		if (chave < elementos[pivo].chave) {
+			return buscarec(chave, esquerda, pivo-1);
+		}
+		if (chave > elementos[pivo].chave) {
+			return buscarec(chave, pivo+1, direita);
+		}
+		return i; // O que é "i" ???
 	}
 
 	void insere(char chave, char valor, int colocacao) {
-		/*  */
+		/* Insere os elementos na lista...?! */
+		for (int i = N; i > colocacao; --i) {
+			elementos[i] = elementos[i-1];
+		
+		elementos[colocacao].chave = chave;
+		elementos[colocacao].valor = valor;
+		
+		N++;
 	}
 
 	void remove(int colocacao) {
-		/*  */
+		/* Remove o elemento da lista ao subtituílo pelo elemento posterior.
+		Realoca toda a lista a partir do elemento removido. */
+		for (int i = colocacao; i < N-1; ++i) {
+			elementos[i] = elementos[i+1];
+		N--;
 	}
 } ListaSeq1;
