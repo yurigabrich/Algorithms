d138b1f04bc6173bd126866b01dbf9933099ef23
Yuri <yuribgabrich@gmail.com>
2017-02-13 18:54:48 -0200

    Create ListaDuplaEnc.c

---
+++ C_files/ListaDuplaEnc.c
@@ -0,0 +1,31 @@
+typedef struct {
+	NoDuploEnc* proximo;
+	NoDuploEnc* anterior;
+	char chave;
+	char valor;
+} NoDuploEnc;
+
+typedef struct {
+	NoDuplaEnc* inicio;
+	int N;
+	
+	void constroi() {
+	
+	}
+	
+	void destroi() {
+	
+	}
+	
+	NoDuplaEnc* busca(char chave) {
+	
+	}
+	
+	void insere(char chave, char valor, NoDuplaEnc* posicao) {
+	
+	}
+	
+	void remove(NoDuplaEnc* posicao) {
+	
+	}
+} ListaDuplaEnc;
