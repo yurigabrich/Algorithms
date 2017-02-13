98128f88d42ef661f250afd20da22fbb262f05e7
Yuri <yuribgabrich@gmail.com>
2017-02-13 10:34:37 -0200

    Update chain_alloc_node_1.py

--- chain_alloc_node_1.py
+++ chain_alloc_node_1.py
@@ -1,24 +1,24 @@
-class Arvore(object):	
-	
-	def __init__(self, raiz):
-	    '''
+class Arvore(object):
+    def __init__(self, raiz):
+        '''
         Classe utilizada para se construir uma estrutura
         de dados não-linear, tipo Árvore.
-	    
-	    Initializa com a definição da raiz.
-	    
-	    Returns: NOTHING
-	    '''
-	    self.raiz = [raiz, []] # [pai, [f1, f2, ...]]
-	    return None
-	    
-	def constroi(self, node, pai = 0):
-	    '''
-	    Constrói a árvore dando-se um valor para o nó e
-	    seu parentesco. Se o índice do pai não for iden-
-	    tificado, o nó será considerado filho da raiz.
-	    '''
-	    self.node = node
+        
+        Initializa com a definição da raiz.
+        
+        Returns: NOTHING
+        '''
+        self.raiz = [raiz, []] # [pai, [f1, f2, ...]]
+        return None
+        
+    def constroi(self, node, pai = 0):
+        '''
+        Constrói a árvore dando-se um valor para o nó e
+        seu parentesco. Se o índice do pai não for iden-
+        tificado, o nó será considerado filho da raiz.
+        '''
+        self.node = node
+        self.pai = pai
 
         if pai == 0:
             self.raiz[1].append(self.node)
@@ -28,19 +28,17 @@ class Arvore(object):
             else:
                 k = self.raiz.index(pai)
                 self.raiz[1][k] = [pai, []]
-            
+        
             pai[1].append[self.node]
-            
+        
         return None
-	
-	def destroi(self):
-	    '''
-	    '''
-	    
-	    return
-	
-	def imprime(self):
-	    '''
-	    '''
-	    
-	    return
+        
+    def destroi(self):
+        '''
+        '''
+        return
+    
+    def imprime(self):
+        '''
+        '''
+        return
