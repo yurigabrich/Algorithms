2e930da071187fa79997ad7519f7cc71ad0f75af
Yuri <yuribgabrich@gmail.com>
2017-02-13 11:31:48 -0200

    função constroi -> OK!
    Alocação encadeada de nós: opção sequencial dos filhos

--- chain_alloc_node_1.py
+++ chain_alloc_node_1.py
@@ -4,32 +4,36 @@ class Arvore(object):
         Classe utilizada para se construir uma estrutura
         de dados não-linear, tipo Árvore.
         
-        Initializa com a definição da raiz.
+        Inicializa com a definição da raiz.
         
         Returns: NOTHING
         '''
         self.raiz = [raiz, []] # [pai, [f1, f2, ...]]
         return None
         
-    def constroi(self, node, pai = 0):
+    def constroi(self, node, pai = None):
         '''
         Constrói a árvore dando-se um valor para o nó e
-        seu parentesco. Se o índice do pai não for iden-
-        tificado, o nó será considerado filho da raiz.
+        seu parentesco. Se o pai não for identificado,
+        o nó será considerado filho da raiz.
         '''
         self.node = node
         self.pai = pai
 
-        if pai == 0:
+        if self.pai == None:
             self.raiz[1].append(self.node)
-        else:
-            if pai not in self.raiz[1]: #na vdd não deveria ser filho de ninguém
-                self.raiz[1].append[pai, []]
-            else:
-                k = self.raiz.index(pai)
-                self.raiz[1][k] = [pai, []]
         
-            pai[1].append[self.node]
+        else:
+            if self.pai == self.raiz[0]: # pai = raiz de uma subárvore
+                self.raiz[1].append(self.node) # constrói a subárvore
+            
+            elif self.pai not in self.raiz[1]:
+                self.raiz[1].append(self.pai)
+            
+            else:
+                k = self.raiz[1].index(pai)
+                self.raiz[1][k] = Arvore(pai)
+                self.raiz[1][k].constroi(self.node, self.pai)
         
         return None
         
@@ -42,3 +46,10 @@ class Arvore(object):
         '''
         '''
         return
+    
+#--------------------------------------
+T = Arvore(4)
+T.constroi(2)
+T.constroi(3)
+T.constroi(5)
+T.constroi(7, 2)
